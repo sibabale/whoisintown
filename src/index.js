96a238c863b58b41997a4633506ce5bc328c1010
sibabale <sibabalejoja@gmail.com>
2023-11-19 09:14:57 +0200

    chore: removed unused resources and formatted code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react';
+
 import './index.css';
 import Dashboard from './views/Dashboard';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './app/store';
 
 import {
